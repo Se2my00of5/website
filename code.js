d35115affa43c7e223705fa50b2f5894c1186b15
Semyon Berezovskiy <ber2semen@gmail.com>
2024-03-23 13:08:13 +0700

    theme v0.3 + пасхалка-авторы

--- code.js
+++ code.js
@@ -11,13 +11,13 @@ themeButton.onclick = () => { //обработка смены темы по на
         themeButton.src=themeButton.src.replace("sun","moon");
        // display.style.backgroundColor="black";
         display.style.background = "linear-gradient(225deg,rgb(41,51,66)9%,rgb(21,31,45)17%,rgb(0,0,0))";
-
+        display.style.color = "white";
     }
     else{
         // перевод на светлую
         themeButton.src=themeButton.src.replace("moon","sun");
         display.style.background="white";
-        
+        display.style.color = "black";
     }
     
 };
